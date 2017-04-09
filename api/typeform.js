57c4f1384d3f9d0d71bf19dbc84a2bb606c547bd
sonphamanh <son021293@gmail.com>
2017-04-09 11:17:38 +0700

    a

--- api/typeform.js
+++ api/typeform.js
@@ -3,7 +3,10 @@ var express = require("express");
 var apiRouter = express.Router();
 
 apiRouter.post("/typeform", (req, res) => {
-   console.log(JSON.stringify(req.body));
+   console.log(JSON.stringify(req));
+   // console.log(JSON.stringify(req.body));
+   console.log('-------');
+   console.log(JSON.stringify(res));
    res.status(200).end();
 });
 
