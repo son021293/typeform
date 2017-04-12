a95f14a20582d0115b16eae4db5a21ac9ded0e7c
sonvn <vungocson94@gmail.com>
2017-04-12 22:05:04 +0700

    change logic

--- src/controllers/typeform.js
+++ src/controllers/typeform.js
@@ -19,6 +19,10 @@ const sheets = {
     C: {
         sheet: "'C' Backend",
         rule: [16, 17, [8, 9], [10, 11, 12, 13, 14, 15], 6, 7, "dateSubmitted", 1]
+    },
+    D: {
+        sheet: "'D' Urgent",
+        rule: [[24, 26], 28, 34, 25, 27, 29, 30, 31, 32, 33, 19, 20, 21, "dateSubmitted", 1]
     }
 };
 
@@ -26,11 +30,11 @@ function applySheetRule(rule, form) {
     let row = [];
 
     rule.forEach((questionNumber) => {
-        if(isArray(questionNumber)) {
+        if (isArray(questionNumber)) {
             row.push(questionNumber.map(q => form[q]).filter(q => q.length > 0).join(", "));
-        } else if(isNumber(questionNumber)) {
+        } else if (isNumber(questionNumber)) {
             row.push(form[questionNumber]);
-        } else if(questionNumber === "dateSubmitted") {
+        } else if (questionNumber === "dateSubmitted") {
             row.push(new Date().toString());
         }
     });
@@ -39,36 +43,52 @@ function applySheetRule(rule, form) {
 }
 
 function getFormRule(form) {
-    if(form["3"] === "Frontend") {
+    if (form["3"] === "Frontend") {
         return sheets["B"];
     } else if (form["3"] === "Backend") {
         return sheets["C"];
     } else if (form["18"].find(i => i === "None of the above")) {
         return sheets["A"];
     } else if (!form["18"].find(i => i === "None of the above")) {
-        return "slack";
+        return sheets["D"];
     }
 }
 
+const listQuestions = [1, [24, 26], 25, 27];
+
 function formatMessageForSlackBot(text) {
+    const questions = text.replace(/\s(\d+\.\s)/g, "\n$1").split(",\n").map(i => {
+        const [question, answer] = (function (questionText) {
+            if (questionText.indexOf("::") >= 0) {
+                const arr = questionText.split("::");
+                return [`${arr[0]}:`, arr[1]];
+            } else {
+                return questionText.split(":")
+            }
+        })(i);
+        return {
+            "title": question,
+            "value": answer,
+            "short": false
+        }
+    });
+
+    const isQuestion = (q, _q) => q.title.indexOf(_q) >= 0;
     return {
         "attachments": [
             {
                 "title": "Development Support",
-                "fields": text.replace(/\s(\d+\.\s)/g, "\n$1").split(",\n").map(i => {
-                    const [question, answer] = (function (questionText) {
-                        if(questionText.indexOf("::") >= 0) {
-                            const arr = questionText.split("::");
-                            return [`${arr[0]}:`, arr[1]];
-                        } else {
-                            return questionText.split(":")
+                "fields": questions.filter(q => {
+                    let isTake = false;
+                    listQuestions.forEach(_q => {
+                        if (isArray(_q) && (isQuestion(q, _q[0]) || isQuestion(q, _q[1]))) {
+                            isTake = true;
+                        } else if (isNumber(_q) && isQuestion(q, _q)) {
+                            isTake = true;
                         }
-                    })(i);
-                    return {
-                        "title": question,
-                        "value": answer,
-                        "short": false
-                    }
+                    });
+
+                    return isTake;
                 })
             }
         ]
@@ -76,7 +96,7 @@ function formatMessageForSlackBot(text) {
 }
 
 @controller("/api/typeform")
-class TypeFormCtrl extends ExpressController{
+class TypeFormCtrl extends ExpressController {
     constructor({googleAuth, sheetId, webHookUrl}) {
         super();
 
@@ -85,24 +105,24 @@ class TypeFormCtrl extends ExpressController{
     }
 
     @post()
-    async post(req, res){
+    async post(req, res) {
         const {fields} = await parseRequest(req);
         const parsedForm = parseForm(JSON.parse(fields.rawRequest));
 
-        submittedForms.push({...parsedForm, pretty: fields.pretty});
+        submittedForms.push({parsedForm, pretty: fields.pretty});
 
         const formRule = getFormRule(parsedForm);
-        if (formRule != "slack") {
-            try {
-                const respone = await this.sheet.insertRow({
-                    range: formRule.sheet,
-                    row: applySheetRule(formRule.rule, parsedForm)
-                })
-            } catch(e) {
-                console.log(e);
+        try {
+            this.sheet.insertRow({
+                range: formRule.sheet,
+                row: applySheetRule(formRule.rule, parsedForm)
+            });
+
+            if (formRule.sheet === "'D' Urgent") {
+                this.slackBot.notify(formatMessageForSlackBot(fields.pretty));
             }
-        } else {
-            this.slackBot.notify(formatMessageForSlackBot(fields.pretty));
+        } catch (e) {
+            console.log(e);
         }
 
         res.json({fields: fields});
